1a0612cac944bcd195523dacdbe1e7c267fe7733
jeremywrags@gmail.com <jragsdale@jragsdale-ltm4.internal.salesforce.com>
2021-12-17 14:59:48 -0600

    added heroku app

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var jsforce = require('jsforce');
+var conn = new jsforce.Connection();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
