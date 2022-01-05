7161b9feab027b29fae41ad5307e8ec478034341
jeremywrags@gmail.com <jragsdale@jragsdale-ltm4.internal.salesforce.com>
2022-01-05 11:39:04 -0600

    My first commit

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "123",
-    DB: "testdb",
+    HOST: "ec2-44-198-146-224.compute-1.amazonaws.com",
+    USER: "azbwkbqrsdorxh",
+    PASSWORD: "444102897ebee71f0f2306a765575e980d91139f7c0fa67bfd264c1cb425e853",
+    DB: "d4r9mltgsocd60",
     dialect: "postgres",
     pool: {
       max: 5,
